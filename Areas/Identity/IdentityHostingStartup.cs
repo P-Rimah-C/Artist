783f45fd8ff306d26dd7a884c7b04b03a497e0fb
P-Rimah-C <amir98327o@mail.ru>
2020-12-08 23:49:46 +0300

    first commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Asernatat_3.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Asernatat_3.Areas.Identity.IdentityHostingStartup))]
+namespace Asernatat_3.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDataContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("AuthDataContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<AuthDataContext>();
+            });
+        }
+    }
+}
